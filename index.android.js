e2c149927eedcca93ec9c2f59b07448fd5dedabf
Silas Jo <52027648+starmaster1126@users.noreply.github.com>
2020-07-12 13:48:44 -0400

    zother

--- index.android.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-import React, { AppRegistry } from 'react-native';
-
-import DashBoard from './components/DashBoard';
-
-AppRegistry.registerComponent('foodDelivery', () => DashBoard);
